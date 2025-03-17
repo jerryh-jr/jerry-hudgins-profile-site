c2e3606e4c3a0a2870a0709608ed10e67df5003f
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2025-03-16 22:59:37 -0500

    Add files via upload.

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import cssIcon from '../assets/css-3.svg';
+import htmlIcon from '../assets/html-5.svg';
+import javascriptIcon from '../assets/javascript.svg';
+import reactIcon from '../assets/react.svg';
+import vueIcon from '../assets/vue.svg';
+import pythonIcon from '../assets/python.svg';
+import pandasIcon from '../assets/pandas-icon.svg';
+import numpyIcon from '../assets/numpy.svg';
+import firebaseIcon from '../assets/firebase.svg';
+import nodeIcon from '../assets/nodejs-icon-alt.svg';
+
+export default function About() {
+    return (
+        <section className="grid md:grid-cols-2 place-items-center mt-10 h-screen v-full">
+            <div className="w-3/4 flex flex-col items-center justify-center lg:bg-gray-300 lg:p-8 lg:rounded-lg lg:drop-shadow-2xl lg:text-gray-950">
+                <h3 className="text-3xl">About me:</h3>
+                <p className="pt-2">
+                    Hey there! I’m a front-end developer passionate about crafting beautiful, responsive websites. Armed with HTML, CSS, JavaScript, and React,
+                    I bring ideas to life on the web. I’m always learning and expanding my skills to stay ahead in this ever-evolving field.
+                    Let’s team up and create something awesome together!
+                </p>
+            </div>
+            <div className="grid grid-cols-3 place-items-center gap-0 w-3/4 mt-10 ">
+                <img className="svg drop-shadow-md " src={javascriptIcon} alt="JavaScript Icon" />
+                <img className="svg drop-shadow-md" src={htmlIcon} alt="html Icon" />
+                <img className="svg drop-shadow-md" src={cssIcon} alt="Css Icon" />
+                <img className="svg drop-shadow-md" src={reactIcon} alt="React Icon" />
+                <img className="svg drop-shadow-md" src={vueIcon} alt="Vue Icon" />
+                <img className="svg drop-shadow-md" src={pythonIcon} alt="Python Icon" />
+                <img className="svg drop-shadow-md" src={pandasIcon} alt="Pandas Icon" />
+                <img className="svg drop-shadow-md" src={numpyIcon} alt="NumPy Icon" />
+                <img className="svg drop-shadow-md" src={firebaseIcon} alt="Firebase Icon" />
+                <img className="svg drop-shadow-md colart-4" src={nodeIcon} alt="Node Icon"/>
+            </div>
+        </section>
+    )
+};
---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Carousel from "react-multi-carousel";
+import projects from '../projectData.js';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+        slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
+
+const carouselItems = projects.map(project => {
+        console.log(project)
+        return (
+            <div className="rounded-md h60 w-3/4 mx-auto my-7 p-2 shadow-2xl bg-gray-200">
+                <a href={project.link}>
+                    <img className="rounded-lg object-contain p-1 shadow-lg" src={project.img} alt={`${project.title} image.`} />
+                </a>
+                <h4 className="pt-2">{project.title}</h4>
+                <p className="text-lg py-2">{project.linkText}</p>
+            </div>
+        )
+    })
+
+export default function Projects() {
+    return (
+        <Carousel
+            responsive={responsive}
+            swipeable={true}
+            draggable={false}
+            showDots={true}
+            infinite={true}
+            keyBoardControl={true}
+            //customTransition="all 0.5"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+        >
+            {carouselItems}
+        </Carousel>
+)
+};
